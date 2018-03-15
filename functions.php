bc5f0a9ec7a9959c4101d10f6abec7eddb02d537
Petter Östergren <petter.ostergren@icloud.com>
2018-03-15 14:14:00 +0100

    feat: full-bleed center-img for small device

--- functions.php
+++ functions.php
@@ -181,7 +181,8 @@ function humescores_scripts() {
 		'expand' => __( 'Expand child menu', 'humescores'),
 		'collapse' => __( 'Collapse child menu', 'humescores'),
 	));
-
+        wp_enqueue_script( 'humescores-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20180315', true );
+        
 	wp_enqueue_script( 'humescores-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
