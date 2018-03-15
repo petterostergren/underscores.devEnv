ee26abfb85f99524393fcd2247bcae9f5fbdfd04
Petter Östergren <petter.ostergren@icloud.com>
2018-03-15 12:50:40 +0100

    refactor: Single-post conntent-flow

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,6 +11,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+                <?php humescores_the_category_list(); ?>
 		<?php
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
@@ -18,7 +19,7 @@
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) : ?>
+		if ( 'post' === get_post_type()  ) : ?>
 		<div class="entry-meta">
 			<?php humescores_posted_on(); ?>
 		</div><!-- .entry-meta -->
