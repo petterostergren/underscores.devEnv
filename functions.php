69344fe85ffb099d922cc9822e448b7aebe18b86
Petter Östergren <petter.ostergren@icloud.com>
2018-03-20 14:03:33 +0100

    style: sticky posts on archive

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ function humescores_setup() {
 	 */
 	add_theme_support( 'post-thumbnails' );
         add_image_size( 'humescores-full-bleed', 2000, 1200, true );
-        add_image_size( 'humescores-index-img', 800, 450, true );
+        add_image_size( 'humescores-index-img', 825, 450, true );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
